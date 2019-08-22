5537cc3a68b21372e1675ea21a0a6d0b8b7e892a
Sunday David <sundaydavid989@gmail.com>
2019-08-22 11:34:25 +0100

    Added Email Verification

--- app/src/main/java/com/example/android/tabianconsulting/ResendVerificationDialog.java
+++ app/src/main/java/com/example/android/tabianconsulting/ResendVerificationDialog.java
@@ -1,14 +1,108 @@
 package com.example.android.tabianconsulting;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 
-public class ResendVerificationDialog extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public class ResendVerificationDialog extends DialogFragment {
+
+    private static final String TAG = "ResendVerificationDialo";
+
+    //widgets
+    private EditText mConfirmPassword, mConfirmEmail;
+
+    //vars
+    private Context mContext;
+
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_resend_verification_dialog);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_resend_verification_dialog, container, false);
+        mConfirmPassword = (EditText) view.findViewById(R.id.confirm_password);
+        mConfirmEmail = (EditText) view.findViewById(R.id.confirm_email);
+        mContext = getActivity();
+
+
+        TextView confirmDialog = (TextView) view.findViewById(R.id.dialogConfirm);
+        confirmDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: attempting to resend verification email.");
+
+                if(!isEmpty(mConfirmEmail.getText().toString())
+                        && !isEmpty(mConfirmPassword.getText().toString())){
+
+
+                }else{
+                    Toast.makeText(mContext, "all fields must be filled out", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+
+        // Cancel button for closing the dialog
+        TextView cancelDialog = (TextView) view.findViewById(R.id.dialogCancel);
+        cancelDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getDialog().dismiss();
+            }
+        });
+
+        return view;
     }
+
+
+
+    /**
+     * sends an email verification link to the user
+     */
+    public void sendVerificationEmail() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user != null) {
+            user.sendEmailVerification()
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(mContext, "Sent Verification Email", Toast.LENGTH_SHORT).show();
+                            }
+                            else{
+                                Toast.makeText(mContext, "couldn't send email", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+
+    }
+
+    /**
+     * Return true if the @param is null
+     * @param string
+     * @return
+     */
+    private boolean isEmpty(String string){
+        return string.equals("");
+    }
+
+
 }
+
+
